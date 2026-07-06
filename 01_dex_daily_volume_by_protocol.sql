0e745eed3b9e686f43f97513f0b1b1f435b9572f
Harshit Dabra <116379749+harshitdabra@users.noreply.github.com>
2026-07-06 18:49:07 +0400

    Add query: DEX daily volume by protocol

---
+++ 01_dex_daily_volume_by_protocol.sql
@@ -0,0 +1,11 @@
+SELECT
+    DATE_TRUNC('day', block_time) AS day,
+        project AS dex_protocol,
+            SUM(amount_usd) AS volume_usd
+            FROM dex.trades
+            WHERE block_time >= NOW() - INTERVAL '90' DAY
+                AND blockchain = 'ethereum'
+                    AND amount_usd IS NOT NULL
+                        AND amount_usd > 0
+                        GROUP BY 1, 2
+                        ORDER BY 1 DESC, 3 DESC
